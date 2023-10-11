c41e88ccb884ac45add43cfbb58c9a1b9f202cbe
Teract10s <trs.leonov@gmail.com>
2023-10-11 17:38:31 +0300

    added book, bookService, bookRepo

---
+++ src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.example.bookstore.repository.impl;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import jakarta.persistence.criteria.CriteriaQuery;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a book: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaQuery<Book> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Book.class);
+            criteriaQuery.from(Book.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books", e);
+        }
+    }
+}
