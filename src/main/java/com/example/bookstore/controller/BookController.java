265f160951e112678a3a87c3294a11eb2ed8f738
Teract10s <trs.leonov@gmail.com>
2023-10-16 12:38:19 +0300

    added update book method & endpoint

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -4,31 +4,48 @@ import com.example.bookstore.dto.BookDto;
 import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.service.BookService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
 public class BookController {
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
-    @GetMapping("/api/books/")
+    @GetMapping("/")
     public List<BookDto> getAll() {
         return bookService.findAll();
     }
 
-    @GetMapping("/api/books/{id}")
+    @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
-    @PostMapping("/api/books/")
+    @PostMapping("/")
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable Long id,
+                              @RequestBody CreateBookRequestDto createBookRequestDto) {
+        return bookService.updateById(createBookRequestDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
 }
--- src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import com.example.bookstore.service.BookService;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,11 +34,46 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    public BookDto updateById(CreateBookRequestDto bookRequestDto, Long id) {
+        Book bookFromDb = bookRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Can't find book by id: " + id));
+        bookFromDb = updateField(bookFromDb, bookRequestDto);
+        return bookMapper.toDto(bookRepository.save(bookFromDb));
+    }
+
+    @Override
     public BookDto findById(Long id) {
         Optional<Book> bookFromDB = bookRepository.findById(id);
         if (bookFromDB.isPresent()) {
             return bookMapper.toDto(bookFromDB.get());
         }
         throw new EntityNotFoundException("Can't find book by id: " + id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    private Book updateField(Book bookFromDb, CreateBookRequestDto bookRequestDto) {
+        if (bookRequestDto.getTitle() != null) {
+            bookFromDb.setTitle(bookRequestDto.getTitle());
+        }
+        if (bookRequestDto.getAuthor() != null) {
+            bookFromDb.setAuthor(bookRequestDto.getAuthor());
+        }
+        if (bookRequestDto.getIsbn() != null) {
+            bookFromDb.setIsbn(bookRequestDto.getIsbn());
+        }
+        if (bookRequestDto.getPrice() != null) {
+            bookFromDb.setPrice(bookRequestDto.getPrice());
+        }
+        if (bookRequestDto.getDescription() != null) {
+            bookFromDb.setDescription(bookRequestDto.getDescription());
+        }
+        if (bookRequestDto.getCoverImage() != null) {
+            bookFromDb.setCoverImage(bookRequestDto.getCoverImage());
+        }
+        return bookFromDb;
     }
 }
