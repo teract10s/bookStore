bfbf780db470883a7cb725e67eb5b18dff274305
Teract10s <trs.leonov@gmail.com>
2023-10-19 16:16:16 +0300

    added EntityNotFound handler

--- src/main/java/com/example/bookstore/exception/ProviderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstore.exception;
-
-public class ProviderNotFoundException extends RuntimeException {
-    public ProviderNotFoundException(String message) {
-        super(message);
-    }
-}
