b25cde26a10ad27d9aea378b8c0c03211d18e11e
Teract10s <trs.leonov@gmail.com>
2023-10-27 00:46:01 +0300

    added category entity and liquibase files for it

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -1,10 +1,14 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @Query("select b from Book b join fetch b.categories c where c.id = :id")
+    List<Book> getBooksByCategoryId(Long id);
 }
