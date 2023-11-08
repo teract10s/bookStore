809ee764027505ec2bf7ff732fb721d0a029036a
Teract10s <trs.leonov@gmail.com>
2023-11-08 12:58:05 +0200

    improved mapping

--- src/main/java/com/example/bookstore/mapper/ShoppingCartMapper.java
+++ src/main/java/com/example/bookstore/mapper/ShoppingCartMapper.java
@@ -1,24 +1,18 @@
 package com.example.bookstore.mapper;
 
-import com.example.bookstore.dto.cart.items.CartItemDto;
 import com.example.bookstore.dto.shopping.cart.ShoppingCartDto;
-import com.example.bookstore.model.CartItem;
 import com.example.bookstore.model.ShoppingCart;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
         nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
-        implementationPackage = "<PACKAGE_NAME>.impl")
+        implementationPackage = "<PACKAGE_NAME>.impl",
+        uses = CartItemMapper.class)
 public interface ShoppingCartMapper {
     @Mapping(source = "user.id", target = "userId")
     ShoppingCartDto toDto(ShoppingCart shoppingCart);
-
-    default CartItemDto toCartItemDto(CartItem cartItem) {
-        return Mappers.getMapper(CartItemMapper.class).toDto(cartItem);
-    }
 }
