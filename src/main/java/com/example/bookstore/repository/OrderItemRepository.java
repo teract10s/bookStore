96c5ac0ed71e25930b0f383a69a6760c180549fd
Teract10s <trs.leonov@gmail.com>
2023-11-10 15:47:52 +0200

    mvn fix

--- src/main/java/com/example/bookstore/repository/OrderItemRepository.java
+++ src/main/java/com/example/bookstore/repository/OrderItemRepository.java
@@ -1,6 +1,5 @@
 package com.example.bookstore.repository;
 
-import com.example.bookstore.model.Order;
 import com.example.bookstore.model.OrderItem;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
